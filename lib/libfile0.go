ede110064a569b1a7614da39e534c74f801069bd
Shihta Kuan <shihta.kuan@gmail.com>
2018-08-17 16:14:41 +0800

    Add lib

---
+++ lib/libfile0.go
@@ -0,0 +1,7 @@
+package lib
+
+import "fmt"
+
+func fun0_1() {
+	fmt.Println("This is libfile0, the fun0_1")
+}
---
+++ lib/libfile1.go
@@ -0,0 +1,7 @@
+package lib
+
+import "fmt"
+
+func fun1_1() {
+	fmt.Println("This is libfile1111111, the fun1_1")
+}
